63bed0fd72b78826c2088a17f43aff6712d0ab28
ionutpantazi <ionut28p@yahoo.com>
2020-03-17 18:10:37 +0000

    update antd

--- src/componente/ListaDeProduse.js
+++ src/componente/ListaDeProduse.js
@@ -1,23 +1,62 @@
 import React from 'react';
-import { Card } from 'antd';
-import { Button } from 'antd';
+import { Row, Col, Card, Tooltip, Modal } from 'antd';
+import { createFromIconfontCN } from '@ant-design/icons';
 class Produse extends React.Component {
+    state = { visible: false };
+    showModal = () => {
+        this.setState({
+          visible: true,
+        });
+    };
+    handleOk = e => {
+        console.log(e);
+        this.setState({
+          visible: false,
+        });
+    };
+    handleCancel = e => {
+        console.log(e);
+        this.setState({
+          visible: false,
+        });
+    };
     render() {
         const { Meta } = Card;
+        const IconFont = createFromIconfontCN({
+            scriptUrl: '//at.alicdn.com/t/font_1697557_irzp4mqx6t8.js',
+          });
         const listaProduse = this.props.produse.map(produs => (
-            <Card
-                hoverable
-                style={{ width: 300, margin: '10px' }}
-                cover={<img src={produs.imagine} alt={produs.nume} style={{ width: '90%', margin: '20px' }}></img>}
-            >
-                <Meta title={produs.nume} description={produs.pret+' lei/kg'}></Meta>
-                <Button type="primary" onClick={(click) => this.props.handleAdauga(click, produs)} style={{ float: 'right'}}>Adauga in cos</Button>
-            </Card>
-            
+            <Col xs={{ span: 12 }} lg={{ span: 8 }} >
+                <Card
+                    hoverable
+                    cover={
+                        <img
+                            src={produs.imagine}
+                            alt={produs.nume}
+                            style={{ width: '80%', margin: '20px' }}
+                        />
+                    }
+                    actions={[
+                        <Tooltip title="adauga in cos">
+                            <IconFont type="icon-iconaddtocart" style={{ fontSize: '30px' }} onClick={(click) => this.props.handleAdauga(click, produs)}/>
+                        </Tooltip>,
+                        <Tooltip title="detalii">
+                            <IconFont type="icon-tariffdetails" style={{ fontSize: '30px' }} onClick={this.showModal}/>
+                        </Tooltip>
+                    ]}  
+                >
+                    <Meta
+                        title={produs.nume}
+                        description={produs.pret+' lei/kg'}
+                    />
+                </Card>
+            </Col>
         ))
         return (
-            <div className="row">
-                {listaProduse}
+            <div>
+                <Row justify="space-around" align="middle" gutter={[24, 24]}>
+                    {listaProduse}
+                </Row>
             </div>
         )
     }
--- src/componente/NumarCos.js
+++ src/componente/NumarCos.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Badge } from 'antd';
+import { createFromIconfontCN } from '@ant-design/icons';
 class Numar extends React.Component {
     render() {
         const { cosProduse } = this.props;
+        const IconFont = createFromIconfontCN({
+            scriptUrl: '//at.alicdn.com/t/font_1697557_vei6hao6e59.js',
+          });
         return (
             <span>
-                <Badge count={cosProduse.length}>
-                <img src="imagini/cos.png" alt="cos" height="60" width="60" />
+                <Badge count={cosProduse.length}> 
+                <IconFont type="icon-Shoppingcartfinancebusiness" style={{ fontSize: '50px' }}/>
+                
                 </Badge>
             </span>
         )
